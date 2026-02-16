44e270fda4e13b725c96f11b1ab70d20b7d21e51
almas-the-fixer <professionalhacker74001@gmail.com>
2026-02-16 12:10:01 +0530

    This sem was mid python was ez rest was bad due to bad instructors..

---
+++ SEM-4/PyThon/Unit - 3/exception.py
@@ -0,0 +1,7 @@
+try:
+    a = input("Enter some input: ")
+    b = input("Enter more input: ")
+    c = input("Enter another input: ")
+except KeyboardInterrupt:
+    print("\nKeyboard Interrupt Exception Occured")
+input("Press any key to exit")
